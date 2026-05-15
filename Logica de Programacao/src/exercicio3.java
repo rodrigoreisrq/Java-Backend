78bbf2c1c1c750aad75bf337a597820a42d05fa1
Rodrigo Reis <rodrigoreis15822@gmail.com>
2026-05-14 22:26:34 -0300

    Implement integer predecessor and successor logic
    Added code to read an integer and print its predecessor and successor.

--- Logica de Programacao/src/exercicio3.java
+++ Logica de Programacao/src/exercicio3.java
@@ -3,6 +3,7 @@ public class exercicio3 {
     public static void main(String[] args) {
         // Desenvolva um algoritmo em Java que leia um número inteiro e imprima o seu
         //antecessor e seu sucessor.
+        
         Scanner scanner = new Scanner(System.in);
         System.out.println("Digite um número: ");
         int number = scanner.nextInt();
