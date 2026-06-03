09a947dd81c4085c79859d905988fd732a0f460c
Rodrigo Reis <rodrigoreis15822@gmail.com>
2026-06-03 16:17:34 -0300

    Fix formatting issues in ExercicioBanco.java

--- MaratonaJava/src/src/ExerciciosProprios/ExercicioBanco.java
+++ MaratonaJava/src/src/ExerciciosProprios/ExercicioBanco.java
@@ -3,7 +3,6 @@ package ExerciciosProprios;
 public class ExercicioBanco {
     public static void main(String[] args) {
         ContaBancaria cb = new ContaBancaria();
-
         cb.titular = "Rodrigo";
         cb.saldo = 500;
         cb.depositar();
